7973e561d8808185277e7315622c2dc35d16e24a
YggdrasziI <37376970+YggdrasziI@users.noreply.github.com>
2022-12-14 19:52:13 +0100

    Update Master 1.18.2-3.1.1

--- src/main/java/fr/yggdraszil/bettershears3/item/ModItems.java
+++ src/main/java/fr/yggdraszil/bettershears3/item/ModItems.java
@@ -21,25 +21,25 @@ public class ModItems
     public static final RegistryObject<Item> WOODEN_SHEARS = ITEMS.register("wooden_shears",
             () -> new BetterShearsTool(new Item.Properties().stacksTo(1).durability(9).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
     public static final RegistryObject<Item> GOLD_SHEARS = ITEMS.register("gold_shears",
-            () -> new Item(new Item.Properties().stacksTo(1).durability(20).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
+            () -> new BetterShearsTool(new Item.Properties().stacksTo(1).durability(20).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
     public static final RegistryObject<Item> FLINT_SHEARS = ITEMS.register("flint_shears",
-            () -> new Item(new Item.Properties().stacksTo(1).durability(35).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
+            () -> new BetterShearsTool(new Item.Properties().stacksTo(1).durability(35).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
     public static final RegistryObject<Item> LAPIS_SHEARS = ITEMS.register("lapis_shears",
-            () -> new Item(new Item.Properties().stacksTo(1).durability(85).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
+            () -> new BetterShearsTool(new Item.Properties().stacksTo(1).durability(85).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
     public static final RegistryObject<Item> CLAY_SHEARS = ITEMS.register("clay_shears",
-            () -> new Item(new Item.Properties().stacksTo(1).durability(101).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
+            () -> new BetterShearsTool(new Item.Properties().stacksTo(1).durability(101).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
     public static final RegistryObject<Item> COPPER_SHEARS = ITEMS.register("copper_shears",
-            () -> new Item(new Item.Properties().stacksTo(1).durability(200).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
+            () -> new BetterShearsTool(new Item.Properties().stacksTo(1).durability(200).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
     public static final RegistryObject<Item> AMETHYST_SHEARS = ITEMS.register("amethyst_shears",
-            () -> new Item(new Item.Properties().stacksTo(1).durability(375).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
+            () -> new BetterShearsTool(new Item.Properties().stacksTo(1).durability(375).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
     public static final RegistryObject<Item> DIAMOND_SHEARS = ITEMS.register("diamond_shears",
-            () -> new Item(new Item.Properties().stacksTo(1).durability(1549).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
+            () -> new BetterShearsTool(new Item.Properties().stacksTo(1).durability(1549).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
     public static final RegistryObject<Item> EMERALD_SHEARS = ITEMS.register("emerald_shears",
-            () -> new Item(new Item.Properties().stacksTo(1).durability(1979).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
+            () -> new BetterShearsTool(new Item.Properties().stacksTo(1).durability(1979).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
     public static final RegistryObject<Item> OBSIDIAN_SHEARS = ITEMS.register("obsidian_shears",
-            () -> new Item(new Item.Properties().stacksTo(1).durability(2999).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
+            () -> new BetterShearsTool(new Item.Properties().stacksTo(1).durability(2999).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
     public static final RegistryObject<Item> NETHERITE_SHEARS = ITEMS.register("netherite_shears",
-            () -> new Item(new Item.Properties().stacksTo(1).durability(4044).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
+            () -> new BetterShearsTool(new Item.Properties().stacksTo(1).durability(4044).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
 
     public static final RegistryObject<Item> STAR_SHEARS = ITEMS.register("star_shears",
             () -> new BetterShearsInfiniteTool(new Item.Properties().stacksTo(1).durability(0).tab(ModCreativeModTab.BETTERSHEARS_TAB)));
